0ffcdf44b61a32e49e5d0e62051be9370b72cf04
Thomas Vogel <Lenex5109@yahoo.com>
2024-09-10 18:54:08 -0500

    edit guest modal & save changes

--- Thomas.js
+++ Thomas.js
@@ -4,7 +4,7 @@ const partyDateEl = document.querySelector("#time-set");
 const editPartySaveBtn = document.querySelector('.edit-party-modal .modal-footer .btn-primary');
 const partyNameModalEl = document.querySelectorAll('.edit-party-modal .modal-body div')[0];
 const addGuestButton = document.querySelector('main > button')
-
+const modalAssignmentsInput = addPersonModal.querySelector();
 
 
 
@@ -15,6 +15,23 @@ addGuestButton.onclick = function() {
     editGuestInfo =false; //reset to adding a new guest
     modalGuestNameInput.value = ''; //clear modal input fields
     addPersonModal.style.display = 'block';
+}
+
+// Open the modal to edit a guest's information
+function openEditGuestModal(guestIndex, guestName, guestAssignments) {
+    editingGuestInfo = true;
+    modalGuestNameInput.value = guestName;  // Prefill guest name
+    modalAssignmentsInput.innerHTML = guestAssignments.map(item => `<li>${item}</li>`).join('');
+    addPersonModal.style.display = 'block';
+
+      // Save changes
+      addPersonModal.querySelector().onclick = function() {
+        const updatedName = modalGuestNameInput.value;
+        const updatedAssignments = Array.from(modalAssignmentsInput.querySelectorAll('li')).map(li => li.textContent);
+        editGuestSPI(guestIndex, updatedName, updatedAssignments);
+        addPersonModal.style.display = 'none';
+        //local storage function;
+    };
 }
 
 //Reset the Party modal
