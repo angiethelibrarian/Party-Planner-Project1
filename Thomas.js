bd7ee9c7b1c1820a008c5e92e40094d7281ee51d
Thomas Vogel <Lenex5109@yahoo.com>
2024-09-10 19:24:44 -0500

    commited out a few things

--- Thomas.js
+++ Thomas.js
@@ -4,7 +4,7 @@ const partyDateEl = document.querySelector("#time-set");
 const editPartySaveBtn = document.querySelector('.edit-party-modal .modal-footer .btn-primary');
 const partyNameModalEl = document.querySelectorAll('.edit-party-modal .modal-body div')[0];
 const addGuestButton = document.querySelector('main > button')
-const modalAssignmentsInput = addPersonModal.querySelector();
+//const modalAssignmentsInput = addPersonModal.querySelector();
 
 
 
@@ -17,22 +17,22 @@ addGuestButton.onclick = function() {
     addPersonModal.style.display = 'block';
 }
 
-// Open the modal to edit a guest's information
-function openEditGuestModal(guestIndex, guestName, guestAssignments) {
-    editingGuestInfo = true;
-    modalGuestNameInput.value = guestName;  // Prefill guest name
-    modalAssignmentsInput.innerHTML = guestAssignments.map(item => `<li>${item}</li>`).join('');
-    addPersonModal.style.display = 'block';
+// // Open the modal to edit a guest's information
+// function openEditGuestModal(guestIndex, guestName, guestAssignments) {
+//     editingGuestInfo = true;
+//     modalGuestNameInput.value = guestName;  // Prefill guest name
+//     modalAssignmentsInput.innerHTML = guestAssignments.map(item => `<li>${item}</li>`).join('');
+//     addPersonModal.style.display = 'block';
 
-      // Save changes
-      addPersonModal.querySelector().onclick = function() {
-        const updatedName = modalGuestNameInput.value;
-        const updatedAssignments = Array.from(modalAssignmentsInput.querySelectorAll('li')).map(li => li.textContent);
-        editGuestSPI(guestIndex, updatedName, updatedAssignments);
-        addPersonModal.style.display = 'none';
-        //local storage function;
-    };
-}
+//       // Save changes
+//       addPersonModal.querySelector().onclick = function() {
+//         const updatedName = modalGuestNameInput.value;
+//         const updatedAssignments = Array.from(modalAssignmentsInput.querySelectorAll('li')).map(li => li.textContent);
+//         editGuestSPI(guestIndex, updatedName, updatedAssignments);
+//         addPersonModal.style.display = 'none';
+//         //local storage function;
+//     };
+// }
 
 //Reset the Party modal
 function resetEditPartyModal() {
